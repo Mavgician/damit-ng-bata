d52a87f48de46a5a7b1a63319b73d484b4c0efa7
Mavgician <jonjon0524@gmail.com>
2024-10-21 16:15:27 +0800

    Added admin page

--- src/app/(standard)/admin-dashboard/page.jsx
+++ src/app/(standard)/admin-dashboard/page.jsx
@@ -1,9 +1,126 @@
 'use client'
 
-export default function Home() {
+import Link from 'next/link';
+
+import {
+  Button,
+  Container,
+  Row,
+  Col
+} from 'reactstrap'
+
+import { useState, useEffect } from 'react';
+
+/* import { getUserCS } from 'firebase-nextjs/client/auth'; */
+import { fetchUserPost } from '@/src/lib/DataServer';
+
+import useSWR from 'swr';
+import Image from 'next/image';
+import { Timestamp } from 'firebase/firestore';
+
+function Order({ orderReference }) {
+  const [order, setOrder] = useState({
+    seat: {
+      type: 'seatType',
+      location_index: [],
+      type_index: 0
+    },
+    claim_type: 'claimType',
+    date: undefined
+  });
+  const [ticket, setTicket] = useState({
+    title: 'ticketTitle',
+    poster_image_url: '',
+    seat: [
+      { price: 0 }
+    ]
+  });
+  const [ID, setID] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    (async () => {
+      const ref = await getDoc(orderReference)
+      setOrder(ref.data())
+      setTicket((await getDoc(ref.data().ticket_ref)).data())
+      setID(ref.data().ticket_ref.id)
+      setLoading(false)
+    })()
+  }, []);
+
   return (
-    <main>
-      <h1>in admin page</h1>
+    !loading &&
+    <Link href={`/tickets/${ID}`} className='bg-light text-dark rounded p-3 border d-block mb-3 text-decoration-none'>
+      <Row>
+        <Col xs={1} sm={1} md={1} lg={1}>
+          <Image src={ticket.poster_image_url} height={0} width={0} sizes='100%' style={{ height: 'auto', width: '100%' }} />
+        </Col>
+        <Col xs={11} sm={11} md={11} lg={11}>
+          <h3 className='m-0'>{ticket.title}</h3>
+          <div className='d-flex mb-2'>
+            <p className='m-0 me-3'>Price: PHP {ticket.seat[order.seat.type_index].price}</p>
+            <p className='m-0 me-3'>Seat: {order.seat.type} - {order.seat.location_index.join(' ')}</p>
+            <p className='m-0 me-3'>Type: {order.claim_type}</p>
+          </div>
+          <p className='text-secondary m-0'>Order ID: {orderReference.id}</p>
+          <p className='text-secondary m-0'>
+            Date: {order.date.toDate().toDateString()} {order.date.toDate().toLocaleTimeString('en-us', { hour: "2-digit", minute: "2-digit", timeZoneName: 'short' })}
+          </p>
+        </Col>
+      </Row>
+    </Link>
+  )
+}
+
+export default function Page() {
+  const { data: user } = useSWR('api/user/verify', fetchUserPost, { suspense: true })
+
+  const [tab, setTab] = useState(0);
+  const [adminTab, setAdminTab] = useState(0);
+
+  const timestamp = new Timestamp(user.creation.seconds, user.creation.nanoseconds)
+
+  return (
+    <main className='bg-light text-dark'>
+      <Container className='p-5' fluid>
+        <h1>Admin Dashboard</h1>
+        <Row>
+          <Col xs={12} s={12} md={2} lg={2}>
+            <div className='my-3'>
+              <Button active={tab === 0} onClick={() => setTab(0)} className='text-start text-secondary' block color='light'>ACCOUNT</Button>
+              <Button active={tab === 1} onClick={() => setTab(1)} className='text-start mt-2 text-secondary' block color='light'>ORDERS</Button>
+            </div>
+          </Col>
+          <Col xs={12} s={12} md={10} lg={10}>
+            <div className={tab === 0 ? '' : 'd-none'}>
+              <h4>Account Overview</h4>
+              
+            </div> 
+            <div className={tab === 1 ? '' : 'd-none'}>
+              <h4>Orders ({user.orders.length})</h4>
+              {user.orders.length > 0 ? user.orders.map(order => <Order key={order.id} orderReference={order} />) : <h3 className='text-secondary'>No orders to see here</h3>}
+            </div>
+            {
+              tab === 2 && <div>
+                <div className='d-flex align-items-center gap-3'>
+                  <h4 className='m-0'>Admin Panel</h4>
+                  <div className="d-flex gap-2">
+                    <Button color='primary' size={'sm'} onClick={() => setAdminTab(0)}>Tickets</Button>
+                    <Button color='primary' size={'sm'} onClick={() => setAdminTab(1)}>Users</Button>
+                  </div>
+                </div>
+                <div className={`py-4 ${adminTab === 0 ? '' : 'd-none'}`}>
+                  
+                </div>
+                <div className={`py-4 ${adminTab === 1 ? '' : 'd-none'}`}>
+                  
+                </div>
+              </div>
+            }
+          </Col>
+        </Row>
+      </Container>
     </main>
-  );
+  )
 }
