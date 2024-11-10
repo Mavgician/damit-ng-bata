a659fe5549e7f0f851f8d98a3be505731b28b0b6
Mavgician <jonjon0524@gmail.com>
2024-11-11 01:25:34 +0800

    Refactored fetchParsed to account for params

--- src/lib/DataServer.ts
+++ src/lib/DataServer.ts
@@ -1,1 +1,11 @@
-export const fetchParsed = (e: string, method: string = 'POST') => fetch(e, {method: method}).then((data) => data.json())
+interface fetchParams {
+  method: string;
+  body: BodyInit | undefined;
+}
+
+export const fetchParsed = async (url: string, fetchParams: fetchParams = {method: 'GET', body: undefined}) => {
+  if (!fetchParams) return ({message: 'fetching'})
+  const { method, body } = fetchParams
+
+  return await fetch(url, {method: method, body}).then(data => data.json())
+}
