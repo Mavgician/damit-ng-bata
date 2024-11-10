a97b4dc96ed18836005da70389c838d4e0c6b7c9
Mavgician <jonjon0524@gmail.com>
2024-11-11 01:27:45 +0800

    Added products API

--- src/app/(standard)/admin-dashboard/page.jsx
+++ src/app/(standard)/admin-dashboard/page.jsx
@@ -6,23 +6,26 @@ import {
   Row,
   Col,
   Table,
-  Input
+  Input,
+  Spinner
 } from 'reactstrap'
 
-import { useContext, useState } from 'react';
-import useSWR from 'swr';
-import { Timestamp } from 'firebase/firestore';
-import { createContext } from 'react';
+import {
+  faTrash,
+  faPen,
+} from '@fortawesome/free-solid-svg-icons'
 
-const fetchUserList = (url, order, limit, firstDoc, lastDoc) => fetch(url, {
-  method: 'POST',
-  body: JSON.stringify({
-    order: order,
-    limit: limit,
-    firstDoc: firstDoc,
-    lastDoc: lastDoc,
-  })
-}).then(data => data.json())
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { useContext, useEffect, useState } from 'react';
+
+import { setDoc, Timestamp } from 'firebase/firestore';
+import { createContext } from 'react';
+import { fetchParsed } from '@/src/lib/DataServer';
+
+import { SetProduct, SetUser } from './modals'
+
+import useSWR from 'swr';
 
 const AdminTableData = createContext(null)
 
@@ -47,10 +50,10 @@ export default function Page() {
               <h5 className='text-secondary'>Statistics here</h5>
             </div>
             <div className={tab === 1 ? '' : 'd-none'}>
-              <AdminModule><Accounts/></AdminModule>
+              <AdminModule url='api/user/list'><Accounts /></AdminModule>
             </div>
             <div className={tab === 2 ? '' : 'd-none'}>
-              <AdminModule url='api/product/list'><Products/></AdminModule>
+              <AdminModule url='api/product/list'><Products /></AdminModule>
             </div>
           </Col>
         </Row>
@@ -59,80 +62,139 @@ export default function Page() {
   )
 }
 
-function TableControls({ setLimit, setOrderBy, next, prev }) {
-  return (
-    <div className='d-flex flex-grow-1 justify-content-end'>
-      <div className='d-flex align-items-center gap-2 me-3'>
-        <p className="m-0">Show</p>
-        <div>
-          <Input
-            type='select'
-            onChange={(e) => { setLimit(Number(e.target.value)) }}
-          >
-            <option value={20}>20</option>
-            <option value={15}>15</option>
-            <option value={10}>10</option>
-            <option value={5}>5</option>
-          </Input>
-        </div>
-      </div>
-      <div className='d-flex align-items-center gap-2 me-3'>
-        <p className="m-0">Sort by</p>
-        <div>
-          <Input
-            type='select'
-            onChange={(e) => { setOrderBy(e.target.value) }}
-          >
-            <option value={'creation'}>Creation</option>
-            <option value={'name'}>Name</option>
-            <option value={'id'}>ID</option>
-            <option value={'email'}>Email</option>
-            <option value={'type'}>Type</option>
-          </Input>
-        </div>
-      </div>
-      <Button color='secondary' className={'ms-1'} onClick={prev}>prev</Button>
-      <Button color='secondary' className={'ms-1'} onClick={next}>next</Button>
-    </div>
-  )
-}
-
-function AdminModule({children, url = 'api/user/list'}) {
+function AdminModule({ children, url }) {
   const [limit, setLimit] = useState(20);
   const [orderBy, setOrderBy] = useState('creation');
 
   const [firstDoc, setFirstDoc] = useState();
   const [lastDoc, setLastDoc] = useState();
-  
-  const { data } = useSWR(
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [modalData, setModalData] = useState(undefined);
+
+  const { data, isLoading } = useSWR(
     [url, orderBy, limit, firstDoc, lastDoc],
-    ([url, order, limit, firstDoc, lastDoc]) => fetchUserList(url, order, limit, firstDoc, lastDoc),
-    { suspense: true }
-  )
+    ([url, order, limit, firstDoc, lastDoc]) => fetchParsed(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        order: order,
+        limit: limit,
+        firstDoc: firstDoc,
+        lastDoc: lastDoc,
+      })
+    }
+    ))
+
+  const maxPage = isNaN(Math.ceil(data?.count / limit)) ? 0 : Math.ceil(data?.count / limit)
+
+  const providerValue = {
+    setLimit,
+    setOrderBy,
+    next,
+    prev,
+    setIsOpen,
+    setModalData,
+    modalData,
+    isOpen,
+    pageNumber,
+    maxPage,
+    data
+  }
 
   function next() {
-    setLastDoc(data.docs[limit - 1])
+    if (pageNumber + 1 == maxPage) return
+
+    setLastDoc(data.data[limit - 1].id)
     setFirstDoc(undefined)
+
+    setPageNumber(pageNumber + 1)
   }
 
   function prev() {
-    setFirstDoc(data.docs[0])
+    if (pageNumber <= 0) return
+
+    setFirstDoc(data.data[0].id)
     setLastDoc(undefined)
+
+    setPageNumber(pageNumber - 1)
   }
 
-  console.log(data);
-  
-  return <AdminTableData.Provider value={{setLimit, setOrderBy, next, prev, data}}>{children}</AdminTableData.Provider>
+  useEffect(() => {
+    setPageNumber(0)
+    setFirstDoc(undefined)
+    setLastDoc(undefined)
+  }, [orderBy]);
+
+  if (isLoading) {
+    return (
+      <div className='w-100 d-flex justify-content-center align-items-center'>
+        <Spinner className='m-5'></Spinner>
+      </div>
+    )
+  }
+
+  return <AdminTableData.Provider value={providerValue}>{children}</AdminTableData.Provider>
+}
+
+function TableControls({ setLimit, setOrderBy, next, prev }) {
+  const { data, pageNumber, maxPage, setIsOpen, isOpen, setModalData } = useContext(AdminTableData)
+
+  function addItem() {
+    setModalData(undefined)
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <>
+      <Button className='ms-3' onClick={addItem}> Add + </Button>
+      <div className='d-flex flex-grow-1 justify-content-end align-items-center'>
+        <div className='d-flex align-items-center gap-2 me-3'>
+          <p className="m-0">Show</p>
+          <div>
+            <Input
+              type='select'
+              onChange={(e) => { setLimit(Number(e.target.value)) }}
+            >
+              <option value={20}>20</option>
+              <option value={15}>15</option>
+              <option value={10}>10</option>
+              <option value={5}>5</option>
+            </Input>
+          </div>
+        </div>
+        <div className='d-flex align-items-center gap-2 me-3'>
+          <p className="m-0">Sort by</p>
+          <div>
+            <Input
+              type='select'
+              onChange={(e) => { setOrderBy(e.target.value) }}
+            >
+              <option value={'creation'}>Creation</option>
+              <option value={'name'}>Name</option>
+              <option value={'id'}>ID</option>
+              <option value={'email'}>Email</option>
+              <option value={'type'}>Type</option>
+            </Input>
+          </div>
+        </div>
+        <p className='m-0' style={{ fontSize: '0.8em' }}>Page {pageNumber + 1} of {maxPage}</p>
+        <Button color='light' className={'ms-1'} onClick={() => prev(data)}>&lt;</Button>
+        <Button color='light' className={'ms-1'} onClick={() => next(data)}>&gt;</Button>
+      </div>
+    </>
+  )
 }
 
 function Accounts() {
-  const {setLimit, setOrderBy, next, prev, data: users} = useContext(AdminTableData)
+  const { setLimit, setOrderBy, next, prev, data: users } = useContext(AdminTableData)
 
   return (
     <>
       <div className="d-flex align-items-center mb-3">
         <h4 className='m-0'>Manage User Accounts</h4>
-        <Button className='ms-3'> Add + </Button>
         <TableControls setLimit={setLimit} setOrderBy={setOrderBy} next={next} prev={prev} />
       </div>
       <Table hover responsive className='p-3'>
@@ -167,10 +229,17 @@ function Accounts() {
                   <span>{data.type}</span>
                 </td>
                 <td className='text-muted'>
-                  <span>View Orders</span>
+                  <Button outline color='success' size='sm' className='mx-1' onClick={() => { setIsOpen(false) }}>
+                    View Orders
+                  </Button>
                 </td>
                 <td className='text-muted'>
-                  <span>action</span>
+                  <Button outline color='danger' size='sm' className='mx-1' onClick={() => { setIsOpen(false) }}>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </Button>
+                  <Button outline color='primary' size='sm' className='mx-1' onClick={() => { setIsOpen(false) }}>
+                    <FontAwesomeIcon icon={faPen} />
+                  </Button>
                 </td>
               </tr>
             )
@@ -182,13 +251,18 @@ function Accounts() {
 }
 
 function Products() {
-  const {setLimit, setOrderBy, next, prev, data: products} = useContext(AdminTableData)
+  const { setLimit, setOrderBy, next, prev, data: products, setIsOpen, isOpen, setModalData } = useContext(AdminTableData)
+
+  function edit(id) {
+    setModalData(id)
+    setIsOpen(!isOpen)
+  }
 
   return (
     <>
+      <SetProduct context={AdminTableData} />
       <div className="d-flex align-items-center mb-3">
         <h4 className='m-0'>Manage Products</h4>
-        <Button className='ms-3'> Add + </Button>
         <TableControls setLimit={setLimit} setOrderBy={setOrderBy} next={next} prev={prev} />
       </div>
       <Table hover responsive className='p-3'>
@@ -196,14 +270,42 @@ function Products() {
           <tr>
             <th className='col-2'>ID</th>
             <th className='col-2'>Product Name</th>
-            <th className='col-2'>Category</th>
+            <th className='col-3'>Cateory</th>
             <th className='col-2'>Created on</th>
-            <th className='col-1'>Type</th>
             <th className='col-1'>Ratings</th>
             <th className='col-2'>Actions</th>
           </tr>
         </thead>
         <tbody>
+          {
+            products.data.map((data, idx) =>
+              <tr key={`user-table-detail-${idx}`}>
+                <td className='text-muted'>
+                  <span>{data.id}</span>
+                </td>
+                <td className='text-muted'>
+                  <span>{data.name}</span>
+                </td>
+                <td className='text-muted'>
+                  <span>{data.tags.join(', ')}</span>
+                </td>
+                <td className='text-muted'>
+                  <span>{(new Timestamp(data.creation.seconds, data.creation.nanoseconds)).toDate().toUTCString()}</span>
+                </td>
+                <td className='text-muted'>
+                  
+                </td>
+                <td className='text-muted'>
+                  <Button outline color='danger' size='sm' className='mx-1' onClick={() => { setIsOpen(false) }}>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </Button>
+                  <Button outline color='primary' size='sm' className='mx-1' onClick={() => { edit(data.id) }}>
+                    <FontAwesomeIcon icon={faPen} />
+                  </Button>
+                </td>
+              </tr>
+            )
+          }
         </tbody>
       </Table>
     </>
--- src/app/api/product/[slug]/route.js
+++ src/app/api/product/[slug]/route.js
@@ -1,5 +1,59 @@
-import { NextResponse } from "next/server";
+import { db } from '@/firebase-app-config.js'
+import { addDoc, collection, doc, getDoc, setDoc, Timestamp } from 'firebase/firestore';
+import { NextResponse } from 'next/server';
+
+import { fetchCollectionItems } from '@/api/fetch_functions'
 
 export async function POST(req, { params }) {
-  return NextResponse.json({ message: 'test' }, { status: 200 })
+  const collectionRef = collection(db, 'products')
+
+  let body, newProductData
+
+  try {
+    body = await req.json()
+
+    newProductData = {
+      carouselurls: [...body.carouselurls],
+      description: body.description,
+      is_available: body.is_available,
+      name: body.name,
+      price: body.price,
+      tags: [...body.tags],
+      thmburl: body.thumbnail,
+      type: [...body.type],
+    }
+  } catch {
+    console.warn('Request body is not set.')
+  }
+
+  try {
+    switch (params.slug) {
+      case 'add':
+        await addDoc(collectionRef, {...newProductData, creation: Timestamp.now(), last_modified: Timestamp.now()})
+        break
+
+      case 'update':
+        await setDoc(doc(collectionRef, body.productID), {...newProductData, last_modified: Timestamp.now()})
+        break
+
+      case 'list':
+        const items = await fetchCollectionItems(collectionRef, body.order, body.limit, body.firstDoc, body.lastDoc)
+
+        return NextResponse.json(items, { status: 200 })
+
+      case 'item':
+        const document = doc(collectionRef, body.productID)
+        const product = (await getDoc(document)).data()
+
+        return NextResponse.json({ ...product }, { status: 200 })
+
+      default:
+        return NextResponse.json({ error: 'Unknown fetch type' }, { status: 501 })
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: 'Wrong request body' }, { status: 500 })
+  }
+
+  return NextResponse.json({ message: 'success' }, { status: 200 })
 }
