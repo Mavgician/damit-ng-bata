a18aac3fe3bebe87059a74e571c8e0a8640c0dbc
Mavgician <jonjon0524@gmail.com>
2024-11-21 12:17:56 +0800

    Added debug for response body and status

--- src/lib/DataServer.ts
+++ src/lib/DataServer.ts
@@ -10,7 +10,9 @@ export const fetchParsed = async (url: string, fetchParams: fetchParams = {metho
   const { method, body } = fetchParams
   const res = await fetch(url, {method: method, body: body, headers: {'Content-Type': 'application/json'}})
 
-  console.log(res);
+  console.log(res.status);
+  console.log(res.body);
+  
 
   try {
     return await res.json()
