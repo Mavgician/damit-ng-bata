6450f03b19aa83336981ec5a19b554c21fe92f9b
Mavgician <jonjon0524@gmail.com>
2024-11-21 12:06:15 +0800

    Changed fetch

--- src/lib/DataServer.ts
+++ src/lib/DataServer.ts
@@ -8,9 +8,12 @@ interface fetchParams {
 export const fetchParsed = async (url: string, fetchParams: fetchParams = {method: 'GET', body: undefined}) => {
   if (!fetchParams) return ({message: 'fetching'})
   const { method, body } = fetchParams
+  const res = await fetch(url, {method: method, body: body, headers: {'Content-Type': 'application/json'}})
+
+  console.log(res);
 
   try {
-    return await fetch(url, {method: method, body: body, headers: {'Content-Type': 'application/json'}}).then(data => data.json())
+    return await res.json()
   } catch (error) {
     console.error(error);
     return NextResponse.json({message: 'Cannot parse response', stack: error}, { status: 500 })
