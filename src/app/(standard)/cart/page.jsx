eb91b80d1de24f5b4911d672c83e7bc55b1cc5a7
Mavgician <jonjon0524@gmail.com>
2024-11-26 01:22:43 +0800

    Added basic functionality

--- src/app/(standard)/cart/page.jsx
+++ src/app/(standard)/cart/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
-/* import { fetchParsed } from "@/src/lib/DataServer"; */
-import { useState } from "react";
+import { fetchParsed } from "@/src/lib/DataServer";
+import { useState, useEffect } from "react";
 import {
   Container,
   Row,
@@ -12,108 +12,116 @@ import {
   CardTitle,
   Input,
 } from "reactstrap";
-/* import useSWR from "swr"; */
+import useSWR from "swr";
+
+import Link from "next/link";
 
 export default function CartPage() {
-  /* const { data: items } = useSWR(['/api/user/cart'], fetchParsed, { suspense: true })
+  const { data: cartItems, isLoading } = useSWR(
+    [`${window.location.origin}/api/user/cart`],
+    ([url]) =>
+      fetchParsed(url,
+        {
+          method: 'POST'
+        }
+      )
+  )
 
-  console.log(items); */
-
-  const [cartItems, setCartItems] = useState([
-    {
-      id: 1,
-      name: "Emré Baby Boy White Blue Christening Romper Set",
-      description: "Sample Description",
-      size: ' M', 
-      price: 1500,
-      quantity: 1,
-      imageUrl: "https://www.myperiwinkle.com/cdn/shop/files/ginee_20241015180455104_6714835123.png?v=1728986897",
-    },
-    {
-      id: 2,
-      name: "Isaac Baby Boy Red Shirt and Shorts Set",
-      description: "Sample Description",
-      size: ' M', 
-      price: 1350,
-      quantity: 1,
-      imageUrl: "https://www.myperiwinkle.com/cdn/shop/files/ginee_20240927172405537_2293938909.png?v=1727429109",
-    },
-    {
-      id: 3,
-      name: "Donnie Baby Boy Light Blue Printed Polo and Shorts Set",
-      description: "Sample Description",
-      size: ' L', 
-      price: 1920,
-      quantity: 1,
-      imageUrl: "https://www.myperiwinkle.com/cdn/shop/files/ginee_20240927153018696_3621042018.png?v=1727422269",
-    }
-  ]);
+  const [itemsTemp, setItemsTemp] = useState([]);
 
   const updateQuantity = (id, newQuantity) => {
-    setCartItems(
-      cartItems.map(item =>
+    setItemsTemp(
+      itemsTemp.map(item =>
         item.id === id ? { ...item, quantity: newQuantity } : item
       )
-    );
-  };
+    )
 
-  const removeItem = id => {
-    setCartItems(cartItems.filter(item => item.id !== id));
-  };
+    fetch(
+      `${window.location.origin}/api/user/update-cart`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          id: id,
+          quantity: newQuantity,
+        })
+      }
+    )
+  }
+
+  const removeItem = (id) => {
+    setItemsTemp(itemsTemp.filter(item => item.id != id));
+
+    fetch(
+      `${window.location.origin}/api/user/remove-cart`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          id: id
+        })
+      }
+    )
+  }
 
   const calculateTotal = () => {
-    return cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
-  };
+    return itemsTemp.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  }
+
+  useEffect(() => {
+    if (!isLoading) {
+      setItemsTemp(cartItems)
+    }
+  }, [isLoading]);
 
   return (
     <main>
       <Container>
         <Row className="my-4">
-          {/* Left Side: Bag */}
           <Col md="8">
-            <h4 className="mb-4">Bag</h4>
-            {cartItems.length > 0 ? (
-              cartItems.map(item => (
-                <div key={item.id} className="d-flex mb-4" style={{ borderBottom: '1px solid #ccc', paddingBottom: '20px' }}>
-                  <img
-                    src={item.imageUrl}
-                    alt={item.name}
-                    style={{ width: '120px', height: '120px', marginRight: '20px', objectFit: 'cover', borderRadius: '8px' }}
-                  />
-                  <div style={{ flexGrow: 1 }}>
-                    <h6>{item.name}</h6>
-                    <p className="text-muted">{item.description}</p>
-                    <p><strong>Size:</strong> {item.size}</p>
-                    <div className="d-flex align-items-center">
-                      <span className="mr-2">Quantity:</span>
-                      <Input
-                        type="number"
-                        min="1"
-                        value={item.quantity}
-                        onChange={e => updateQuantity(item.id, parseInt(e.target.value))}
-                        style={{ width: '70px', marginLeft: '10px' }}
+            <h4 className="mb-4">Your cart</h4>
+            {
+              itemsTemp.length > 0 ? (
+                itemsTemp.map(item => (
+                  <div key={item.id} className="d-flex mb-4" style={{ borderBottom: '1px solid #ccc', paddingBottom: '20px' }}>
+                    <Link href={`${window.location.origin}/products/${item.id}`} className="d-inline">
+                      <img
+                        src={item.imageUrl}
+                        alt={item.name}
+                        style={{ width: '120px', height: '120px', marginRight: '20px', objectFit: 'cover', borderRadius: '8px' }}
                       />
+                    </Link>
+                    <div style={{ flexGrow: 1 }}>
+                      <h6>{item.name}</h6>
+                      <p className="text-muted">{item.description}</p>
+                      <p><strong>Size:</strong> {item.size}</p>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">Quantity:</span>
+                        <Input
+                          type="number"
+                          min="1"
+                          value={item.quantity}
+                          onChange={e => updateQuantity(item.id, parseInt(e.target.value))}
+                          style={{ width: '70px', marginLeft: '10px' }}
+                        />
+                      </div>
+                      <div className="d-flex justify-content-between mt-3">
+
+                        <Button
+                          color="dark"
+                          onClick={() => removeItem(item.id)}
+                          style={{ padding: '0.5rem 3rem' }}
+                        >
+                          Remove
+                        </Button>
+                      </div>
                     </div>
-                    <div className="d-flex justify-content-between mt-3">
-      
-                      <Button
-                   color="dark"
-                   onClick={() => removeItem(item.id)}
-                    style={{ padding: '0.5rem 3rem' }}
-                    >
-                  Remove
-                  </Button>
-                    </div>
+                    <h6 className="text-right" style={{ minWidth: '80px' }}>P{(item.price * item.quantity).toFixed(2)}</h6>
                   </div>
-                  <h6 className="text-right" style={{ minWidth: '80px' }}>P{(item.price * item.quantity).toFixed(2)}</h6>
-                </div>
-              ))
-      
-            ) : (
-              <p>Your cart is empty.</p>
-            )}
+                ))
+
+              ) : (
+                <p>Your cart is empty.</p>
+              )}
           </Col>
-          {/* Right Side: Summary */}
           <Col md="4">
             <Card>
               <CardBody>
@@ -139,12 +147,12 @@ export default function CartPage() {
                   Checkout
                 </Button>
                 <Button color="light" block style={{ display: 'flex', alignItems: 'center' }}>
-        <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/PayPal.svg/800px-PayPal.svg.png?20230314142951"
-      alt="PayPal"
-      style={{ width: '150px', height: '40px', marginRight: '8px' }} 
-        />
-      </Button>
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/PayPal.svg/800px-PayPal.svg.png?20230314142951"
+                    alt="PayPal"
+                    style={{ width: '150px', height: '40px', marginRight: '8px' }}
+                  />
+                </Button>
               </CardBody>
             </Card>
           </Col>
