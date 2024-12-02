4b7f0fdb9f9017918e2cdfb7012501cd7c131832
Mavgician <jonjon0524@gmail.com>
2024-12-03 04:30:01 +0800

    Added content for about us

--- src/app/(standard)/about-us/page.jsx
+++ src/app/(standard)/about-us/page.jsx
@@ -1,10 +1,47 @@
 'use client'
 
+import { Container } from "reactstrap";
+
 export default function Page() {
 
   return (
-    <main>
-      
+    <main className="d-flex justify-content-center align-items-center">
+      <Container>
+        <div className="my-5">
+          <h3>ABOUT US</h3>
+          <p className="text-justify">
+            <b>DNB</b> was established on the tenets of simplicity, modernity, and an unwavering dedication to quality. Our primary goal is to provide parents with affordable clothing for their children without compromising quality. We firmly believe in the harmonious blend of quality, efficiency, and simplicity, woven into the very foundation of our company.
+          </p>
+          <p>
+            With an emphasis on customer satisfaction and secure transactions, we offer a comprehensive range of childrens attire. At Damit ng Bata, our ethos revolves around a steadfast dedication to modernity. We invest in the latest technology and ensure our platforms stay updated with industry advancients. This dedication ensures effective, precise, and state-of-the-art clothe selling services, guaranteeing optimal experiences for our customers. Quality is the cornerstone of DNB; we understand that every transaction, no matter how simple, deserves the utmost care.
+          </p>
+          <p>
+            Our team of knowledgeable and experienced professionals upholds the highest standards of service at DNB. We take immense pride in our meticulous processes, fostering trust and satisfaction in a secure and efficient environment.
+          </p>
+          <p>
+            In addition to our commitment to excellence in clothe selling, we value community engagement and regularly participate in events aimed at promoting affordability for parents. We believe in contributing to the cultural vibrancy of our community and supporting initiatives that enhance the overall experience of parents. DNB is not just a children&apos;  s apparel selling platform; it&apos;s a part of the rich history of parenting.
+          </p>
+        </div>
+        <div className="my-5">
+          <h3>OUR MISSION</h3>
+          <p>
+            &quot;<i>At <b>DNB</b>,</i> our mission is to <b>enhance the life and overall enjoyment</b>
+            of parents by providing <i>exceptional apparel services</i> in a customer-focused
+            environment. We are dedicated to offering a wide range of children&apos;s apparel for various uses and
+            situations, ensuring that everyone can find the perfect experience.
+          </p>
+          <p>
+            Our team of skilled professionals is committed to providing seamless clothing solutions that prioritize
+            convenience, reliability, and customer satisfaction. Through continuous innovation and a commitment to
+            excellence, we aim to enhance the lives of parents and children.
+          </p>
+          <p>
+            Our commitment extends beyond sales; we aim to <b>engage with our customers</b> and
+            <b>enhance their overall experience</b> through personalized recommendations and
+            customer support.
+          </p>
+        </div>
+      </Container>
     </main>
   );
 }
