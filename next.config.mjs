ae438f118e2cbf9192d89c7e0bca8306247735c7
Mavgician <jonjon0524@gmail.com>
2024-12-03 02:49:53 +0800

    Added anchor for secure localhost

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import autoCert from "anchor-pki/auto-cert/integrations/next"
+
+const withAutoCert = autoCert({
+  enabledEnv: "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withAutoCert(nextConfig);
