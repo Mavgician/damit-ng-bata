cb0c3fe184ab562ae98421a1e7aaecaad727246b
Mavgician <jonjon0524@gmail.com>
2024-12-04 04:37:57 +0800

    Fixed ESLint errors for deployment

--- src/app/(standard)/account-disabled/page.jsx
+++ src/app/(standard)/account-disabled/page.jsx
@@ -2,7 +2,7 @@ export default function Page( ) {
   return (
     <main className="d-flex justify-content-center align-items-center">
       <div>
-        <h1>You're account is currently disabled. You do not have access to the webpage.</h1>
+        <h1>You&apos;re account is currently disabled. You do not have access to the webpage.</h1>
         <p className="text-center">
           Please contact site admins if you think this is a mistake.
         </p>
