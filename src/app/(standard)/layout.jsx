3bf66b955cc03aaedd02e65a68ab4eb55283623f
Mavgician <jonjon0524@gmail.com>
2024-12-12 19:18:14 +0800

    Add Analytics

--- src/app/(standard)/layout.jsx
+++ src/app/(standard)/layout.jsx
@@ -2,12 +2,14 @@
 
 import { Navigationbar } from '@/components/Navigation';
 import { Footer } from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout({children}) {
   return (
     <>
       <Navigationbar />
         {children}
+        <Analytics/>
       <Footer/>
     </>
   );
